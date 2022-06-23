6394c161984477f0997c2d293bfdefb22d097e19
vidityar adith <van.adith@gmail.com>
2022-06-23 12:36:13 +0700

    Update Dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,15 @@
 <template>
   <v-app>
-    <v-navigation-drawer 
-      v-model="drawer"
-      fixed
-      app
-      color="#FEFEFE"
-    >
-      <v-list-item>
-        <div class="d-flex align-center pa-52px">
-          <v-list-item-content>
-            <v-img
-            alt="Gym Management Logo"
-            contain
-            src="@/assets/Logo.png"
-            height="127px"
-            width="212px"
-          />
-          </v-list-item-content>
-        </div>
-      </v-list-item>
-
-
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="(item, index) in items"
-          :key="index"
-          @click="press(item.title)"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    
-    <v-app-bar
-      app
-      color="#FEFEFE"
-      fixed
-    >
-    <v-app-bar-nav-icon 
-      @click.stop="drawer = !drawer"
-      color="#F48743">
-    </v-app-bar-nav-icon>
-
-      <div class="d-flex align-center">
-        <v-app-bar-title
-        class="font-weight-bold"
-        > GYM MEMBERSHIP SYSTEM </v-app-bar-title>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view />
-    </v-main>
+      <v-main>
+        <router-view />
+      </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
+  // components: { NavBarDataKelas },
   name: 'App',
-
-    data: () => ({
-      items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Data Admin', icon: 'mdi-badge-account-horizontal-outline' },
-        { title: 'Data Member', icon: 'mdi-badge-account-horizontal-outline' },
-        { title: 'Classes', icon: 'mdi-dumbbell' },
-        { title: 'Newsletter', icon: 'mdi-newspaper-variant' },
-        { title: 'Logout', icon: 'mdi-logout' },
-      ],
-      drawer: false,
-      group: null,
-    }),
-
-    watch: {
-      group () {
-        this.drawer = false
-      },
-    },
   }
 </script>
 
@@ -106,14 +18,18 @@ export default {
     background-color: #FEE9CC;
 }
 
-.v-image__image, .v-image__placeholder {
-    max-width: 70%;
-}
-
 * {
     justify-content: center;
+    font-weight: 600;
 }
 
+.navIcon{
+  max-height: 40px;
+  max-width: 224px;
+}
 
-
+.expansion .v-expansion-panels{
+  max-height: 40px;
+  max-width: 224px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,76 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Dashboard from "@/views/Dashboard.vue"
+import DataKelasOffline from "@/views/DataKelasOffline.vue"
+import DataAdmin from "@/views/DataAdmin.vue"
+import DataMember from "@/views/DataMember.vue"
+import DataPemesanan from "@/views/DataPemesanan.vue"
+import DataPelatih from "@/views/DataPelatih.vue"
+import Berita from "@/views/Berita.vue"
+import JadwalLatihan from "@/views/JadwalLatihan.vue"
+import Login from "@/views/LoginView.vue";
+import DataKelasOnline from "@/views/DataKelasOnline.vue";
+import Navigasi from "@/components/Navigasi.vue"
 
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: "/",
-        name: "Dashboard",
-        component: Dashboard,
+        component: Navigasi,
+        children:[{
+            path: "/",
+            name: "Dashboard",
+            component: Dashboard,
+        },
+        {
+            path: "/DataKelasOffline",
+            name: "Data Kelas Offline",
+            component: DataKelasOffline,
+        },
+        {
+            path: "/DataKelasOnline",
+            name: "Data Kelas Online",
+            component: DataKelasOnline,
+        },
+        {
+            path: "/DataAdmin",
+            name: "Data Admin",
+            component: DataAdmin,
+        },
+        {
+            path: "/DataMember",
+            name: "Data Member",
+            component: DataMember,
+        },
+        {
+            path: "/DataPelatih",
+            name: "Data Pelatih",
+            component: DataPelatih,
+        },
+        {
+            path: "/DataPemesanan",
+            name: "Data Pemesanan",
+            component: DataPemesanan,
+        },
+        {
+            path: "/JadwalLatihan",
+            name: "Jadwal Latihan",
+            component: JadwalLatihan,
+        },
+        {
+            path: "/Berita",
+            name: "Berita",
+            component: Berita,
+        },
+        ]
+    },
+    {
+        path: "/Login",
+        name: "Login",
+        component: Login,
+        children: [
+        ]
     },
 ];
 
@@ -17,5 +79,13 @@ const router = new VueRouter({
     base: process.env.BASE_URL,
     routes,
 });
+
+
+router.beforeEach((to, from, next) =>{
+    const isAuthenticated = JSON.parse(localStorage.getItem('authenticated'));
+    if(to.name !== "Login" && !isAuthenticated) next({ name: "Login"});
+    if(to.name === "Login" && isAuthenticated) next({ name: "Dashboard"});
+    else next();
+})
 
 export default router;
